ed11e8b6c103ba9674d291825ed4c104fa27e765
Hixus <lenaicb22@gmail.com>
2025-01-22 09:55:11 +0100

    Fix controller class name

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TestController extends AbstractController
+class ApiController extends AbstractController
 {
     #[Route('/api/test', name: 'api_test', methods: ['GET'])]
     public function test(): JsonResponse
